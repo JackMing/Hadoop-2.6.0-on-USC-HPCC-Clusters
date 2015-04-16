0c96acb7baba32fc3eefd68e9401d5ab257cfa6d
Jack Ming <mingyihs@usc.edu>
2015-04-16 01:00:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hadoop-2.6.0-on-USC-HPCC-Clusters
---
+++ setup.sh
@@ -0,0 +1,21 @@
+if [ ! "${MINGENV_HADOOP}" ] ; then
+  MINGENV_HADOOP=1 ; export MINGENV_HADOOP
+
+  # HADOOP_VERSION=1.1.2
+  JAVA_PATH=/usr/lib/jvm/jre-1.6.0-openjdk.x86_64
+  JAVA_HOME=/usr/lib/jvm/jre-1.6.0-openjdk.x86_64
+
+  # prefix=/usr/usc/hadoop/default
+  prefix=/home/rcf-40/mingyihs/hadoop-proj/hadoop
+  _bindir=$prefix/bin
+
+  # export HADOOP_VERSION
+  export JAVA_HOME
+  if [ -n "$PATH" ]; then
+    PATH=$_bindir:$JAVA_PATH:$PATH
+  else
+    PATH=$_bindir:$JAVA_PATH
+  fi
+  export PATH
+
+fi
