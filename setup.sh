38e1424162a61d22494503c7e847023a7d097b91
Ming-Yi Hsiao <mingyihs@usc.edu>
2015-04-23 16:25:00 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ if [ ! "${MINGENV_HADOOP}" ] ; then
   JAVA_PATH=/usr/lib/jvm/jre-1.6.0-openjdk.x86_64
   JAVA_HOME=/usr/lib/jvm/jre-1.6.0-openjdk.x86_64
 
-  # prefix=/usr/usc/hadoop/default
-  prefix=/path/to/hadoop/home/directory/
-  _bindir=$prefix/bin
+  # HADOOP_HOME=/usr/usc/hadoop/default
+  HADOOP_HOME=/path/to/hadoop/home/directory/
+  _bindir=$HADOOP_HOME/bin
 
   # export HADOOP_VERSION
   export JAVA_HOME
